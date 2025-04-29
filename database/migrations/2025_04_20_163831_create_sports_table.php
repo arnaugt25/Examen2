56889889e16ce7252b79dc2a9ffb572f1fa84726
arnaugt25 <agarcia3@cendrassos.net>
2025-04-29 15:14:29 +0200

    edicion migration

--- database/migrations/2025_04_20_163831_create_sports_table.php
+++ database/migrations/2025_04_20_163831_create_sports_table.php
@@ -11,13 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::('sports', function (Blueprint $table) {
+        Schema::create('sports', function(Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->text('description');
             $table->foreignId('category_id');
             $table->timestamps();
         });
+
     }
 
     /**
@@ -25,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('sports');
-    }
+        //
+    }   
 };
